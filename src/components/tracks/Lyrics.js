530067c6224671df5b96ba17298628276c8e4b13
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-09-21 19:16:23 +0100

    Fixes

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -11,11 +11,11 @@ class Lyrics extends Component {
     } 
 
     componentDidMount(){
-        axios.get(`http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
         .then(res => {
             // console.log(res.data);
             this.setState({lyrics : res.data.message.body.lyrics});
-            return   axios.get(`http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
+            return   axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
       
         })
         .then( res => {
